cb9f3d8c837bcb2e5cc0b51a8de381cbbc435389
Andrey Rybakov <rybakov.ad@icloud.com>
2023-10-08 00:46:32 +0200

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ sys.path.insert(0, abspath("."))
 
 
 # Project information
-project = "Custom hooks for pre-commit"
+project = "Custom hooks"
 author = "Andrey Rybakov"
 copyright = f"2023-{datetime.now().year}, {author}"
 
@@ -83,8 +83,23 @@ html_theme_options = {
     "dark_css_variables": {
         "color-brand-primary": "#7BC4AC",
     },
-    "navigation_with_keys": True,
+    "source_repository": "https://github.com/adrybakov/pre-commit-hooks",
+    "source_branch": "main",
+    "source_directory": "docs/",
     "top_of_page_button": "edit",
+    "navigation_with_keys": True,
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/adrybakov/pre-commit-hooks",
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,
+            "class": "",
+        },
+    ],
 }
 
 # fix problem with autosummary and numpydoc:
@@ -104,3 +119,16 @@ rst_epilog = "\n".join(
     map(lambda x: f".. |{x}| replace:: {frozen_locals[x]}", variables_to_export)
 )
 del frozen_locals
+
+# Custom substitutions for links. Solution source:
+# https://docutils.sourceforge.io/docs/ref/rst/directives.html#directives-for-substitution-definitions
+custom_links = {
+    "pre-commit": ("pre-commit", "https://pre-commit.com"),
+}
+
+rst_epilog += "\n".join(
+    map(
+        lambda x: f"\n.. |{x}| replace:: {custom_links[x][0]}\n.. _{x}: {custom_links[x][1]}",
+        [i for i in custom_links],
+    )
+)
--- docs/index.rst
+++ docs/index.rst
@@ -1,6 +1,6 @@
-******************
-Custom pre-commits
-******************
+******************************
+Custom hooks for |pre-commit|_
+******************************
 
 For the description of each hook see corresponding page:
 

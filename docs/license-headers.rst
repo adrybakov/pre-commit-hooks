080bb63865de40105b8e6702376022c55a92da54
Andrey Rybakov <rybakov.ad@icloud.com>
2025-08-06 19:22:33 +0200

    Fix bugs and remove unnecessary placeholders

--- docs/license-headers.rst
+++ docs/license-headers.rst
@@ -19,18 +19,12 @@ Configuration
 .. code-block:: yaml
 
     - repo: https://github.com/adrybakov/pre-commit-hooks
-      rev: 0.3.0                          # Check available tags on the GitHub page
+      rev: 0.3.0                  # Check available tags on the GitHub page
       hooks:
-        - id: license-headers             # Required
-          args:                           # Optional (as the next lines)
-            - --license-template          # If license summary is not in a "LICENSE" file
-            - LICENSE.template            # Path to the license summary file
-            - --start-year                # If you use {{start-year}} in the template
-            - 2021                        # Value for {{start-year}}
-            - --author-name               # If you use {{author-name}} in the template
-            - Name Surname Extra Name     # Value for {{author-name}} (any amount of words)
-            - --author-email              # If you use {{author-email}} in the template
-            - some-email@some-address.com # Value for {{author-e-mail}}
+        - id: license-headers     # Required
+          args:                   # Optional (as the next lines)
+            - --license-template  # If license summary is not in a "LICENSE" file
+            - LICENSE.template    # Path to the license summary file
 
 See `pre-commit docs <https://pre-commit.com/index.html#pre-commit-configyaml---hooks>`_
 for the list of additional parameters.
@@ -40,14 +34,11 @@ License template
 
 File with the license template support some dynamic placeholders:
 
-============================= ============================= =============================
-Placeholder                   User-provided value           Generated value
-============================= ============================= =============================
-``{{start-year}}``            Value of ``--start-year``     Unapplicable
-``{{current-year}}``          Unapplicable                  ``datetime.now().year``
-``{{author-name}}``           Value of ``--author-name``    Unapplicable
-``{{author-email}}``          Value of ``--author-email``   Unapplicable
-============================= ============================= =============================
+============================= =============================
+Placeholder                   Generated value
+============================= =============================
+``{{current-year}}``          ``datetime.now().year``
+============================= =============================
 
 An example template is
 
@@ -62,7 +53,7 @@ The above example will be equivalent to the following code (assuming that you
 
 .. code-block:: bash
 
-    license-headers some-filename-1.py some-filename-2.py --license-template LICENSE.template --start-year 2021 --author-name Name Surname Extra Name --author-email some-email@some-address.com
+    license-headers some-filename-1.py some-filename-2.py --license-template LICENSE.template
 
 Implementation details
 ======================
--- pre_commit_hooks/license_headers.py
+++ pre_commit_hooks/license_headers.py
@@ -1,6 +1,7 @@
+# ================================== LICENSE ===================================
 # MIT License
 #
-# Copyright (c) 2023-2025 Andrey Rybakov (rybakov.ad@icloud.com)
+# Copyright (c) 2023 - 2025 Andrey Rybakov (rybakov.ad@icloud.com)
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -19,8 +20,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
-
+#
+# ================================ END LICENSE =================================
 from argparse import ArgumentParser
 from datetime import datetime
 
@@ -28,9 +29,6 @@ from datetime import datetime
 def ensure_license(
     filenames,
     license_template,
-    start_year=None,
-    author_name=None,
-    author_email=None,
     verbose=False,
 ):
     r"""
@@ -46,13 +44,6 @@ def ensure_license(
         List of file to which the license is added.
     license_template : list of str
         Path to the file with the template of the license.
-    start_year : str
-        Value for the placeholder ``{{start-year}}`` in the license template.
-    author_name : list of str
-        Value for the placeholder ``{{author-name}}`` in the license template.
-        Used as ``author_name = " ".join(author_name)``.
-    author-email : str
-        Value for the placeholder ``{{author-email}}`` in the license template.
     """
 
     license_start = f"# {' LICENSE ':=^78}\n"
@@ -64,32 +55,8 @@ def ensure_license(
 
     # Update placeholders with the data
     def get_data(placeholder_name):
-        if placeholder_name == "start-year":
-            if start_year is None:
-                raise ValueError(
-                    "start-year is not provided, "
-                    "please add an argument to the pre-commit config."
-                )
-            return start_year
-
         if placeholder_name == "current-year":
             return str(datetime.now().year)
-
-        if placeholder_name == "author-name":
-            if author_name is None:
-                raise ValueError(
-                    "author-name is not provided, "
-                    "please add an argument to the pre-commit config."
-                )
-            return " ".join(author_name)
-
-        if placeholder_name == "author-email":
-            if author_email is None:
-                raise ValueError(
-                    "author-email is not provided, "
-                    "please add an argument to the pre-commit config."
-                )
-            return author_email
 
         raise ValueError(f'Placeholder "{placeholder_name}" is not supported.')
 
@@ -117,7 +84,16 @@ def ensure_license(
         tmp_text.append(get_data(text_portion[0]))
         tmp_text.append(text_portion[1])
 
-    license_text = "# " + "\n# ".join("".join(tmp_text).split("\n")) + "\n"
+    # Add comment symbols at the beginning
+    tmp_text = "".join(tmp_text).split("\n")
+
+    for i in range(len(tmp_text)):
+        if len(tmp_text[i]) == 0:
+            tmp_text[i] = "#"
+        else:
+            tmp_text[i] = "# " + tmp_text[i]
+
+    license_text = "\n".join(tmp_text) + "\n"
 
     # Insert license text into the files
     for file in filenames:
@@ -145,7 +121,8 @@ def ensure_license(
         else:
             raise ValueError(
                 "License end and start are inconsistent. "
-                f"Start index: {license_start_index}, end index: {license_end_index}."
+                f"Start index: {license_start_index}, end index: {license_end_index}. "
+                f"File {file}"
             )
 
         with open(file, "w", encoding="utf-8") as f:
@@ -167,25 +144,6 @@ def main():
         "--license-template",
         default="LICENSE",
         help="Path to the template file of the license",
-    )
-    parser.add_argument(
-        "--start-year",
-        type=str,
-        default=None,
-        help="One of the data placeholders for the license template.",
-    )
-    parser.add_argument(
-        "--author-name",
-        type=str,
-        default=None,
-        nargs="*",
-        help="One of the data placeholders for the license template.",
-    )
-    parser.add_argument(
-        "--author-email",
-        type=str,
-        default=None,
-        help="One of the data placeholders for the license template.",
     )
     parser.add_argument(
         "-v",
